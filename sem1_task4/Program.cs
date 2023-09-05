26bbfb74b5b089360468fbe49b9434127450321e
MaksimMaksimov <m.maksimovxviii@gmail.com>
2023-09-05 18:40:57 +0300

    added tasks condition to files and solution task8

--- sem1_task4/Program.cs
+++ sem1_task4/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите первое число: ");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
 int firstNum = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int secondNum = Convert.ToInt32(Console.ReadLine());
--- sem1_task8/Program.cs
+++ sem1_task8/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+if (count < 2)
+{
+    Console.WriteLine("Ошибка входных данных. Введенное число меньше 2. Чётных чисел нет");
+}
+else
+{
+    if (count % 2 == 0)
+    {
+        for (int i = 2; i <= count; i += 2)
+        {
+            Console.Write("{0} ", i);
+        }
+    }
+    else
+    {
+        for (int i = 2; i <= count - 1; i += 2)
+        {
+            Console.Write("{0} ", i);
+        }
+    }
+}
