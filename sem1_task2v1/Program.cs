7272c5e483f38e20afd7ea6403469dff7b2fa1af
MaksimMaksimov <m.maksimovxviii@gmail.com>
2023-09-05 17:06:26 +0300

    added second version of solution task2

--- sem1_task2v1/Program.cs
+++ sem1_task2v1/Program.cs
@@ -1,1 +1,16 @@
 ﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру (Решение через массив)
+Console.Write("Введите номер дня недели: ");
+int numOfDay = Convert.ToInt32(Console.ReadLine());
+string[] dayOfWeek = new string[7];
+
+dayOfWeek[0] = "Понедельник";
+dayOfWeek[1] = "Вторник";
+dayOfWeek[2] = "Среда";
+dayOfWeek[3] = "Четверг";
+dayOfWeek[4] = "Пятница";
+dayOfWeek[5] = "Суббота";
+dayOfWeek[6] = "Воскресенье";
+
+Console.WriteLine(numOfDay < 1 || numOfDay > 7 ?
+    "Такого дня недели не существует" :
+    dayOfWeek[numOfDay - 1]);
