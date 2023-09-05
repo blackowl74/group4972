b6832d483c5405df18977e08c14ee4820acff7d8
MaksimMaksimov <m.maksimovxviii@gmail.com>
2023-09-05 18:22:02 +0300

    added solution task4

--- sem1_task4/Program.cs
+++ sem1_task4/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNum;
+
+if (secondNum > max)
+{
+    max = secondNum;
+}
+if (thirdNum > max)
+{
+    max = thirdNum;
+}
+Console.WriteLine("Максимальное число: {0}", max);
