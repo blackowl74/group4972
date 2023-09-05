715f8d35cc4434e998f0f1537f555c0d5e66b87b
MaksimMaksimov <m.maksimovxviii@gmail.com>
2023-09-05 18:29:59 +0300

    added solution task6

--- sem1_task6/Program.cs
+++ sem1_task6/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.Write("Введите число: ");
+int numOddOrEven = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numOddOrEven % 2 == 0 ? "Число {0} чётное" :
+    "Число {0} нечётное",
+     numOddOrEven);
