c6a1ed097994033c86b0282ddbb82314676e2783
Maksim <m.maksimovxviii@gmail.com>
2023-09-07 21:33:59 +0300

    added solution task12

--- sem2_task12/Program.cs
+++ sem2_task12/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число некратно первому, то программа выводит остаток от деления.
+
+int GetNum(){
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int firstNum = GetNum();
+int secondNum = GetNum();
+
+Console.WriteLine(secondNum % firstNum == 0 ? "Кратное" : secondNum % firstNum);
+
+
