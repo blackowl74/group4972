d8e225e640c98fdbe0cffe19573500068c223359
Maksim <m.maksimovxviii@gmail.com>
2023-09-07 21:21:01 +0300

    added solution task11

--- sem2_task11/Program.cs
+++ sem2_task11/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+using System;
+
+int getRandomNum(int min, int max)
+{
+    int randomNum = new Random().Next(min, max);
+    return randomNum;
+}
+
+int num = getRandomNum(99, 1000);
+
+int firstDigit = num / 100;
+int lastDigit = num % 10;
+
+Console.WriteLine(num);
+int resNumber = firstDigit*10 + lastDigit;
+Console.WriteLine(resNumber);
