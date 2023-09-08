f35bf843d360f27588800f79d2fe702235f0b318
Maksim <m.maksimovxviii@gmail.com>
2023-09-08 19:03:42 +0300

    added solution task10 and rework task9

--- sem2_task0/Program.cs
+++ sem2_task0/Program.cs
@@ -1,31 +1,28 @@
 ﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
 using System;
 
-void maxDigitOfRandomNum(){
+int getRandomNum(int min, int max)
+{
+    int randomNum = new Random().Next(min, max);
+    return randomNum;
+}
+
+void maxDigitOfRandomNum()
+{
     Console.WriteLine("Способ через число");
-
-
-    int getRandomNum(int min, int max){
-        int randomNum = new Random().Next(min, max);
-        return randomNum;
-    }
     int num = getRandomNum(10, 100);
     Console.WriteLine(num);
-    Console.WriteLine(num / 10 > num%10 ? num /10 : num%10);
+    Console.WriteLine(num / 10 > num % 10 ? num / 10 : num % 10);
 }
 
 maxDigitOfRandomNum();
 
-void maxDigitOfRandomNumFromChar(){
+void maxDigitOfRandomNumFromChar()
+{
     Console.WriteLine("Способ через символы");
-    int getRandomNum(int min, int max){
-        int randomNum = new Random().Next(min, max);
-        return randomNum;
-    }
     int num = getRandomNum(10, 100);
     char[] digitChar = num.ToString().ToCharArray();
-
-    Console.WriteLine((int)digitChar[0]>(int)digitChar[1]?digitChar[0]:digitChar[1]);
+    Console.WriteLine((int)digitChar[0] > (int)digitChar[1] ? digitChar[0] : digitChar[1]);
 }
 
 maxDigitOfRandomNumFromChar();
--- sem2_task10/Program.cs
+++ sem2_task10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int GetNum(){
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int num = GetNum();
+bool result = num > 99 && num < 1000;
+
+Console.WriteLine(result ? "Вторая цифра числа: {0}" : "Число не трёхзначное", num / 10 % 10 );
