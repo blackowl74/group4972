dd05b3016077a1386c36347950745cd8daad256a
Maksim <m.maksimovxviii@gmail.com>
2023-09-08 19:50:39 +0300

    added solution task13

--- sem2_task13/Program.cs
+++ sem2_task13/Program.cs
@@ -1,4 +1,26 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// * Сделать вариант для числа длиной до 10 цифр не используя char или string
+using System;
 
 
+// Метод для получения числа
+int GetNum()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+// Получаем число и номер разряда
+Console.Write("Введите число: ");
+int num = GetNum();
+Console.Write("Введите номер разряд: ");
+int digit = GetNum();
+// Метод для получения числа искомого разряда
+int GetDigit(int num, int numOfDigit)
+{
+    while (num > Math.Pow(10, numOfDigit))
+    {
+        num /= 10;
+    }
+    return num = num % 10;
+}
+Console.WriteLine(num < Math.Pow(10, digit - 1) ? "Число меньше" : GetDigit(num, digit));
 
