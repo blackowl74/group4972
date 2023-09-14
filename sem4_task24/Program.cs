4d5f7887fb1f9e5717c6c4552a4de8365d6cabe7
Maksim <m.maksimovxviii@gmail.com>
2023-09-14 20:42:46 +0300

    added solution task24

--- sem4_task24/Program.cs
+++ sem4_task24/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int GetNum(){
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int sumOfNumbers(int num){
+    int sum = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+void Output(string message){
+    Console.WriteLine(message, sumOfNumbers(GetNum()));
+}
+Output("Сумма чисел введенного числа: {0}");
