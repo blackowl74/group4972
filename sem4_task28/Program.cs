b0efb78788824e32dfbf40e3308454f1e9167f45
Maksim <m.maksimovxviii@gmail.com>
2023-09-14 21:36:38 +0300

    added solution task28

--- sem4_task28/Program.cs
+++ sem4_task28/Program.cs
@@ -1,3 +1,23 @@
 ﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+using System.Numerics;
+int GetLongNum()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+BigInteger GetFactorial(int num)
+{
+    BigInteger fact = 1;
+    for (int i = 0; i < num; i++)
+    {
+        fact *= num;
+    }
+    return fact;
+}
 
+void Output(string message, BigInteger num){
+    Console.WriteLine(message, num);
+}
+
+Output("Факториал числа: {0}", GetFactorial(GetLongNum()));
