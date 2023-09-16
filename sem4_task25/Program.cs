82dd4afe73988be9ccda11919b15a8e80f51cfb6
Maksim <m.maksimovxviii@gmail.com>
2023-09-16 20:38:40 +0300

    added comment to task25

--- sem4_task25/Program.cs
+++ sem4_task25/Program.cs
@@ -3,38 +3,45 @@
 // 2, 4 -> 16
 //Написать калькулятор с операциями +, -, /, * и возведение в степень
 
+
+// Метод для получения числа от пользователя
 int GetNum(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
 
+// Метод для получения оператора от пользователя
 char GetOperator(string message)
 {
     Console.Write(message);
     return Convert.ToChar(Console.ReadLine());
 }
 
+// Метод сложения
 int ToSum(int firstNum, int secondNum)
 {
     return firstNum + secondNum;
 }
-
+// Метод вычитания
 int ToSubtraction(int firstNum, int secondNum)
 {
     return firstNum - secondNum;
 }
 
+// Метод умножения
 int ToMultiply(int firstNum, int secondNum)
 {
     return firstNum * secondNum;
 }
 
+// Метод деления, возвращает тип double, т.к. при делении могут получиться не только целые числа
 double ToDivide(int divisible, int divider)
 {
     return (double)divisible / (double)divider;
 }
 
+// Метод возведения в степень
 int ToPow(int num, int degree)
 {
     int pow = 1;
@@ -44,10 +51,13 @@ int ToPow(int num, int degree)
     }
     return pow;
 }
+// Метод для более удобного вывода ответа
+void OutputResult(object operation){
+    Console.WriteLine("Ответ: {0}", operation);
+}
 
-// Todo:
-// Добавить третий параметр string для получения операции от пользователя
-// Вместо "Введите степень" - "Введите второе число"
+
+// Метод, который проверяет знак и, в зависимости от знака, вызывает нужный метод и выдает ответ
 void Calculation(string firstNumber, string secondNumber, string oper)
 {
     int firstNum = GetNum($"{firstNumber}: ");
@@ -56,19 +66,19 @@ void Calculation(string firstNumber, string secondNumber, string oper)
     switch (operat)
     {
         case '+':
-            Output(ToSum(firstNum, secondNum));
+            OutputResult(ToSum(firstNum, secondNum));
             break;
         case '-':
-            Output(ToSubtraction(firstNum, secondNum));
+            OutputResult(ToSubtraction(firstNum, secondNum));
             break;
         case '*':
-            Output(ToMultiply(firstNum, secondNum));
+            OutputResult(ToMultiply(firstNum, secondNum));
             break;
         case '/':
-            Output(ToDivide(firstNum, secondNum));
+            OutputResult(ToDivide(firstNum, secondNum));
             break;
         case '^':
-            Output(ToPow(firstNum, secondNum));
+            OutputResult(ToPow(firstNum, secondNum));
             break;
         default:
             Console.WriteLine("Данной операции не существует");
@@ -77,9 +87,7 @@ void Calculation(string firstNumber, string secondNumber, string oper)
 
 }
 
-void Output(object operation){
-    Console.WriteLine("Ответ: {0}", operation);
-}
+
 
 Calculation("Введите первое число", "Введите второе число", "Введите оператор");
 
