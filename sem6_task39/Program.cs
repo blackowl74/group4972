826208782dd04fd84eb6b00fe4893f2f16e879c8
MaksimMaksimov <m.maksimovxviii@gmail.com>
2023-09-26 14:51:14 +0300

    added solution task39

--- sem6_task39/Program.cs
+++ sem6_task39/Program.cs
@@ -2,3 +2,59 @@
 
 //     [1 2 3 4 5] -> [5 4 3 2 1]
 //     [6 7 3 6] -> [6 3 7 6]
+
+
+int GetNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetRandomInt(int min, int max)
+{
+    int randomNum = new Random().Next(min, max);
+    return randomNum;
+}
+
+int[] GenArray(int num, int min, int max)
+{
+    int[] array = new int[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = GetRandomInt(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write("{0}, ", array[i]);
+    }
+    Console.WriteLine("{0}]", array[array.Length - 1]);
+}
+
+void ReverseArray(int[] array){
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
+}
+
+void Output(string message, int[] array)
+{
+    PrintArray(array);
+    Console.WriteLine(message);
+    ReverseArray(array);
+    PrintArray(array);
+}
+
+int[] randomArray = GenArray(GetNum("Введите размерность массива: "), GetNum("Введите минимальное значение элемента: "), GetNum("Введите максимальное значение элемента: "));
+Output("Реверсивный массив: ", randomArray);
+
+
