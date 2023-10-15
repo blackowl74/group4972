6eb68656d10474bde19674a52a9c46f7e4676a71
Maksim Maksimov <m.maksimovxviii@gmail.com>
2023-10-15 17:29:04 +0300

    add solution task66

--- sem9_task66/Program.cs
+++ sem9_task66/Program.cs
@@ -2,4 +2,25 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int GetNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+int SumRangeRec(int numM, int numN)
+{
+    if (numM == numN)
+    {
+        return numN;
+    }
+    else
+    {
+        return numM + SumRangeRec(numM + 1, numN);
+    }
+}
+
+int numberM = GetNum("Введите число М: ");
+int numberN = GetNum("Введите число N: ");
+
+Console.WriteLine($"Сумма натуральных чисел в промежутке равна {SumRangeRec(numberM, numberN)}");
